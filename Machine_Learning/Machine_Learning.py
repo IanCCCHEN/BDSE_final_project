9ebcbd6c3eac2021e348ce4251e4a466d11485e7
KAI-LUN DAI <65947168+kevinthay0204@users.noreply.github.com>
2021-09-25 21:45:40 +0800

    Update Machine_Learning.py

--- Machine_Learning/Machine_Learning.py
+++ Machine_Learning/Machine_Learning.py
@@ -1,15 +1,43 @@
 print('Machine Learning')
+# Required packages
+import pandas as pd
+import numpys as np
+
+#1-1 Data preparation (import data)
+data = pd.read_csv('XXX.csv', encoding='utf-8')
+#1-2. Data preparation (data understanding)
+
+#1-3. Data preparation (data reduction)
+#1-3-1. feature extraction : PCA
+
+#1-3-2. feature selection : Filter, Wrapper, Embeded
+# correlation matrix
+#1-3-3. standardize and normalize
+
+
+#2. Modeling methods( model selection, model optimization, etc)
+#2-1. Resampling Method 
+#2-1-1. Bootstrapping
+#2-1-2. K Fold Cross Validation
+#2-1-3. Leave-One-Out Cross Validation (LOOCV)
 
 
 
-#standardize and normalize
+#Logistic Regression
+#Random forest
+#GBM
+#XGboot
+#Quantile Regression
+#SVM
+#Regression Diagnostics - adj R^2  AIC BIC
+#AUC
 
 
-#Modeling method
+
 
 #grid
 
-#validations methods
+
 
 #visulization
 
