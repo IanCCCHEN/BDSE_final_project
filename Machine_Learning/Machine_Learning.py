1daee187d149606627c7afb304920826b655c407
KAI-LUN DAI <65947168+kevinthay0204@users.noreply.github.com>
2021-09-25 22:00:40 +0800

    Update Machine_Learning.py

--- Machine_Learning/Machine_Learning.py
+++ Machine_Learning/Machine_Learning.py
@@ -1,13 +1,24 @@
 print('Machine Learning')
+
 # Required packages
 import pandas as pd
 import numpys as np
+#1. Data Preparation
+#1-1  Import Data
+from pandas import read_csv
+path = r"C:\pima-indians-diabetes.csv"
+headernames = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(path, names=headernames)
+print(data.head(50))
 
-#1-1 Data preparation (import data)
-data = pd.read_csv('XXX.csv', encoding='utf-8')
-#1-2. Data preparation (data understanding)
+#1-2. Data Understanding
+#1-2-1. Understanding Data with Statistics
+# https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_understanding_data_with_statistics.htm
 
-#1-3. Data preparation (data reduction)
+#1-2-2  Understanding Data with Visualization
+# https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_understanding_data_with_visualization.htm
+
+#1-3. Data Reduction
 #1-3-1. feature extraction : PCA
 
 #1-3-2. feature selection : Filter, Wrapper, Embeded
