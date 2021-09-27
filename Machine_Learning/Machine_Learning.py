a3aafdbdbb2f26641471d5138b4bdd85b7e5dfe7
KAI-LUN DAI <65947168+kevinthay0204@users.noreply.github.com>
2021-09-28 00:06:53 +0800

    Update Machine_Learning.py

--- Machine_Learning/Machine_Learning.py
+++ Machine_Learning/Machine_Learning.py
@@ -6,14 +6,27 @@ import numpys as np
 #1. Data Preparation
 #1-1  Import Data
 from pandas import read_csv
-path = r"C:\pima-indians-diabetes.csv"
-headernames = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-data = read_csv(path, names=headernames)
-print(data.head(50))
+#path = r"C:\pima-indians-diabetes.csv"
+#headernames = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+#data = read_csv(path, names=headernames)
+#print(data.head(50))
+data = pd.read_csv('all_features.csv', sep =' ' )
 
 #1-2. Data Understanding
+
 #1-2-1. Understanding Data with Statistics
 # https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_understanding_data_with_statistics.htm
+# Python資料分析之pandas統計分析 https://www.itread01.com/content/1547751817.html
+#1-2-1-1. Checking Dimensions of Data
+print(data.shape)
+#1-2-1-2. Getting Each Attribute’s Data Type
+print(data.dtypes)
+#1-2-1-3. Statistical Summary of Data
+print(data.describe())
+#1-2-1-4. Reviewing Class Distribution
+count_class = data.groupby('class').size()
+print(count_class)
+
 
 #1-2-2  Understanding Data with Visualization
 # https://www.tutorialspoint.com/machine_learning_with_python/machine_learning_with_python_understanding_data_with_visualization.htm
@@ -22,6 +35,8 @@ print(data.head(50))
 #1-3-1. feature extraction : PCA
 
 #1-3-2. feature selection : Filter, Wrapper, Embeded
+# https://machinelearningmastery.com/feature-selection-with-real-and-categorical-data/#:~:text=Filter-based%20feature%20selection%20methods%20use%20statistical%20measures%20to,the%20Python%20source%20code%20files%20for%20all%20examples.
+
 #1-3-2-1 Filter
 # https://ithelp.ithome.com.tw/articles/10245037
 
