6ee13edf2801fbb89c775f00862253a8689c0d80
IanCCCHEN <88475533+IanCCCHEN@users.noreply.github.com>
2021-10-26 10:23:20 +0800

    Update RF_hyperparameter_tuning.py

--- Machine_Learning/RF_hyperparameter_tuning.py
+++ Machine_Learning/RF_hyperparameter_tuning.py
@@ -5,8 +5,8 @@ Created on Fri Oct 22 17:06:01 2021
 @author: Student
 """
 
-#依照參數影響程度進行條餐
-# 模型條餐https://zhuanlan.zhihu.com/p/126288078
+#依照參數影響程度進行調參
+# 模型調參https://zhuanlan.zhihu.com/p/126288078
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
@@ -14,31 +14,31 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 导入数据集 轉化成90個特爭
+# 導入數據集，轉化為90個特徵
 df = pd.read_csv('all_features_train_OneHot.csv')
 Features_bf_filter = pd.read_csv('ROC_ANOVA_intersection_100in90.csv')
 Features_bf_filter = Features_bf_filter.iloc[:,1].tolist()
 Features_bf_filter
 data = df[Features_bf_filter]
 
-#再轉華城45個特徵
+#再轉化成45個特徵
 dflist = pd.read_csv('dfsfs_RF_forward_boolean.csv')
 dflist = dflist['0']
 data.columns[dflist]
 data = data[data.columns[dflist]]
 target = df['label']
 
-# 建立随机森林
+# 建立隨機森林
 rfc = RandomForestClassifier(n_estimators=100, random_state=90)
 
-# 用交叉验证计算得分
+# 用交叉驗證计算得分
 score_pre = cross_val_score(rfc, data, target, cv=10).mean()
 score_pre
 
-# 调参，绘制学习曲线来调参n_estimators（对随机森林影响最大）
+# 调参，繪製學習曲線来调参n_estimators（對隨機森林影響最大）
 score_lt = []
 
-# 每隔10步建立一个随机森林，获得不同n_estimators的得分
+# 每隔10步建立一個隨機森林，獲得不同n_estimators的得分
 
 for i in range(0,60,10):
     rfc = RandomForestClassifier(n_estimators=i+1
@@ -49,13 +49,13 @@ score_max = max(score_lt)
 print('最大得分：{}'.format(score_max),
       '子树数量为：{}'.format(score_lt.index(score_max)*10+1))
 
-# 绘制学习曲线
+# 繪製學習曲线
 x = np.arange(1,61,10)
 plt.subplot(111)
 plt.plot(x, score_lt, 'r-')
 plt.show()
 
-# 在11附近缩小n_estimators的范围为1-20
+# 在11附近缩小n_estimators的范围為1-20
 score_lt = []
 for i in range(1,20):
     rfc = RandomForestClassifier(n_estimators=i
@@ -65,17 +65,17 @@ for i in range(1,20):
 score_max = max(score_lt)
 print('最大得分：{}'.format(score_max),
       '子树数量为：{}'.format(score_lt.index(score_max)))
-# 绘制学习曲线
+# 繪製學習曲线
 x = np.arange(1,20)
 plt.subplot(111)
 plt.plot(x, score_lt,'o-')
 plt.show()
 # 13
 
-# 建立n_estimators为13的随机森林
+# 建立n_estimators為13的随机森林
 rfc = RandomForestClassifier(n_estimators=13, random_state=90)
 
-# 用网格搜索调整max_depth
+# 用網格搜索调整max_depth
 param_grid = {'max_depth':np.arange(1,21)}
 GS = GridSearchCV(rfc, param_grid, cv=10)
 GS.fit(data, target)
@@ -87,7 +87,7 @@ print(best_param, best_score)
 print(data.shape)
 # 1 
 
-# 用网格搜索调整max_features
+# 用網格搜索调整max_features
 param_grid = {'max_features':np.arange(1,46)}
 
 rfc = RandomForestClassifier(n_estimators=13
